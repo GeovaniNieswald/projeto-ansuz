48ab9096d8064c45404ae94316bf2e00e25c3d6b
GeovaniNieswald <geovaninieswald@gmail.com>
2020-04-05 23:33:19 -0300

    Mostrando valores na tela

--- ansuz/hugin/hugin.py
+++ ansuz/hugin/hugin.py
@@ -1,17 +1,49 @@
 import tkinter as tk
+import random
 
 class Hugin():
 
+    mensagens_positivas = ['Tudo tranquilo', 'Tudo certo', 'Parabéns você está dentro do limite de velocidade', 'É isso ai']
+    mensagens_negativas = ['Acima do limite de velocidade', 'Atenção você está acima do limite de velocidade', 'Você está colocando a sua e a vida dos outros em risco', 'Devagar cabeça']
+
     def __init__(self, limite_velocidade):
         self.limite_velocidade = limite_velocidade
+
         self.window = tk.Tk()
+        self.window.title('Módulo Hugin')
+        self.window.geometry('500x500')
+        self.window.resizable(0, 0)
+        
+        self.frame = tk.Frame(self.window, bg='black', width=500, height=500)
+        self.frame.grid(row=0, column=0, sticky="NW")
+        self.frame.grid_propagate(0)
+        self.frame.update()
+
+        self.label_velocidade = tk.Label(self.frame, text="", bg='black', fg='red')
+        self.label_velocidade.place(x=250, y=230, anchor='center')
+
+        self.label_mensagem = tk.Label(self.frame, text="", bg='black', fg='red')
+        self.label_mensagem.place(x=250, y=250, anchor='center')
 
     def processa_velocidade(self, velocidade):
-        mensagem = ""
+        mensagem = ''
+        cor = ''
         if self.limite_velocidade >= velocidade:
-            mensagem = "Tudo tranquilo"
+            mensagem = Hugin.mensagens_positivas[self.__msg_rand()]
+            cor = 'green'
         else:
-            mensagem = "Acima do limite de velocidade"
+            mensagem = Hugin.mensagens_negativas[self.__msg_rand()]
+            cor = 'red'
 
-        greeting = tk.Label(text=mensagem)
-        greeting.pack()
+        self.label_velocidade.configure(text=velocidade, fg=cor) 
+        self.label_mensagem.configure(text=mensagem, fg=cor)
+
+    def zera_painel(self):
+        self.label_velocidade.configure(text='') 
+        self.label_mensagem.configure(text='')
+
+    def __msg_rand(self):
+        limite = len(Hugin.mensagens_negativas) - 1
+        rand_num = random.randint(0, limite)
+ 
+        return rand_num
--- ansuz/main.py
+++ ansuz/main.py
@@ -14,4 +14,64 @@ if __name__ == '__main__':
             else: 
                 break 
         except ValueError:
-            print("Velocidade deve ser um numero inteiro.")            
+            print("Velocidade deve ser um numero inteiro.") 
+
+"""
+from threading import Thread
+import time
+from hugin.hugin import *
+from odin.odin import *
+
+hugin = Hugin(50)
+odin = Odin(0)
+
+tempo_ultima_velocidade = 0
+
+rodar = True
+
+def thread1(threadname):
+    global hugin
+    global odin
+    global tempo_ultima_velocidade
+    global rodar
+
+    while True:
+        if rodar == False:
+            break
+
+        if tempo_ultima_velocidade >= 10:
+            hugin.zera_painel()
+            tempo_ultima_velocidade = 0
+        else:
+            tempo_ultima_velocidade += 1
+            time.sleep(1)
+
+def thread2(threadname):
+    global hugin
+    global odin
+    global tempo_ultima_velocidade
+    global rodar
+
+    while True:
+        if rodar == False:
+            break
+
+        try:
+            odin.set_velocidade(int(input("Velocidade: ")))
+
+            if odin.velocidade > 0:
+                hugin.processa_velocidade(odin.velocidade)
+                tempo_ultima_velocidade = 0
+            else: 
+                rodar = False
+        except ValueError:
+            print("Velocidade deve ser um numero inteiro.") 
+
+thread1 = Thread( target=thread1, args=("Thread-1", ) )
+thread2 = Thread( target=thread2, args=("Thread-2", ) )
+
+thread1.start()
+thread2.start()
+
+thread2.join()
+"""
